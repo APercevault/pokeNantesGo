29d74688b02645883a98983ffca76f7b3c3927ce
Alain Percevault <alainpercevaultfrance@gmail.com>
2020-08-27 16:49:16 +0200

    creation de la BDD v.1

---
+++ src/Controller/mainController.php
@@ -0,0 +1,19 @@
+<?php 
+// src/Controller/mainController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class mainController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function home()
+    {
+        return $this->render('home/index.html.twig');
+        }
+    }
+
